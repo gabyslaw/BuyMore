e07c68a704a2b6ff743774e40fe31e69e8294501
lord jj gud <olowoniwa.ayokunle@gmail.com>
2023-01-03 01:39:01 +0000

    Added Seed users to Identity table and resolved the AddScoped issue on program.cs

--- BuyMore.Services.Identity/Program.cs
+++ BuyMore.Services.Identity/Program.cs
@@ -1,4 +1,8 @@
+using BuyMore.Services.Identity;
 using BuyMore.Services.Identity.DbContexts;
+using BuyMore.Services.Identity.Initializer;
+using BuyMore.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +11,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>()
+.AddDeveloperSigningCredential();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 
 var app = builder.Build();
 
@@ -23,7 +45,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseIdentityServer();
+
 app.UseAuthorization();
+
+//var dbInitializer = app.Services.GetService<IDbInitializer>();
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    var myDependency = services.GetRequiredService<IDbInitializer>();
+
+    //Use the service
+    myDependency.Initialize();
+
+}
 
 app.MapControllerRoute(
     name: "default",
